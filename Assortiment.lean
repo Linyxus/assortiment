5b538e0452efac6d30aac2f50f3f70bfc6d073df
Yichen Xu <yichen.x@outlook.com>
2024-09-19 12:08:08 +0200

    add import

--- Assortiment.lean
+++ Assortiment.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Assortiment` library.
 -- Import modules here that should be built as part of the library.
 import Assortiment.Basic
+import Assortiment.CMCC
